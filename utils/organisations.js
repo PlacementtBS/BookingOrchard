f2c81e3b66f2231d5cf65f37d3fc4ad3bc848281
Ben Scott <ben@placementt.co.uk>
2025-07-15 23:44:00 +0100

    Colour and branding update

--- utils/organisations.js
+++ utils/organisations.js
@@ -1,34 +1,42 @@
 import { insert, select, update } from "./db.js";
 import createAccount from "./auth.js";
 
-export default async function createOrganisation(name, ufname, usname, uemail, upassword){
-    const acc = await createAccount(ufname,usname,uemail,upassword);
-        const users = await select("users", "*", {
-          column: "email",
-          operator: "eq",
-          value: uemail
-        });
-    
-        const user = users[0];
-    if (user){
-        insert("organisations", {
-            name: name,
-            admin: user.id
-        },)
-    
-    const organisations = await select("organisations", "*", {
-        column: "admin",
-        operator: "eq",
-        value: user.id
-    },)
-    const organisation = organisations[0];
-    if(organisation){
-        update("users",{organisationId:organisation.id},{
-            column: "id",
-            operator: "eq",
-            value: user.id
-        })
-    }
-    }
+export default async function createOrganisation(name, ufname, usname, uemail, upassword) {
+  try {
+    const acc = await createAccount(ufname, usname, uemail, upassword);
 
+    const users = await select("users", "*", {
+      column: "email",
+      operator: "eq",
+      value: uemail
+    });
+
+    const user = users[0];
+    if (!user) throw new Error("Failed to find created user");
+
+    await insert("organisations", {
+      name: name,
+      admin: user.id
+    });
+
+    const organisations = await select("organisations", "*", {
+      column: "admin",
+      operator: "eq",
+      value: user.id
+    });
+
+    const organisation = organisations[0];
+    if (!organisation) throw new Error("Failed to find created organisation");
+
+    await update("users", { organisationId: organisation.id }, {
+      column: "id",
+      operator: "eq",
+      value: user.id
+    });
+
+    return { organisation, user };
+  } catch (err) {
+    console.error("createOrganisation error:", err);
+    throw err;
+  }
 }
